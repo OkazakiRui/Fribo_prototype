1b3b2ee637aa48821c9b21d743a20909a49f60f6
RuiOkazaki <okazaki200226@gmail.com>
2021-09-25 00:42:11 +0900

    修正

--- Desktop/reactStudy/section2/06-props/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders SLAM DUNK characters', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/花道/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- Desktop/reactStudy/section2/08-lifecycle/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders timer', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/タイマー/);
-  expect(linkElement).toBeInTheDocument();
-});
